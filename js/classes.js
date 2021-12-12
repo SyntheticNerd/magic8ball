acfef1d8d2973f2f01b2f65cbdff8a71c944dfb2
SyntheticDesigner <andrew.sch.j@gmail.com>
2021-12-12 03:21:30 -0800

    add the form and ball class tied in functionality

--- js/classes.js
+++ js/classes.js
@@ -69,6 +69,110 @@ class JSCheckBox {
     }
 }
 
-class JSForm {
+class JSInput {
+    constructor(
+        _element,
+        _labelText,
+        _colorString,
+        _width,
+        _height,
+        _fontSize
+    ) {
+        this.element = _element;
+        this.labelText = _labelText;
+        this.color = _colorString;
+        this.width = _width;
+        this.height = _height;
+        this.fontSize = _fontSize;
+        this.inputID = this.setID();
+        this.value = "";
+        this.keyCounter = 0;
+        this.input = document.createElement("input");
+        this.label = document.createElement("label");
+        this.labelContent = document.createElement("span");
+    }
+    initialize() {
+        this.element.appendChild(this.label);
+        this.element.appendChild(this.input);
+        this.label.appendChild(this.labelContent);
+        this.input.setAttribute("id", `${this.inputID}`);
+        this.label.setAttribute("for", `${this.inputID}`);
+        this.labelContent.innerHTML = this.labelText;
+        this.value = this.input.value;
+        this.eventHandler();
+        this.styleDefault();
+    }
+    styleDefault() {
+        this.input.style.color = this.color;
+        this.input.style.border = "none";
+        this.input.style.outline = "none";
+        this.input.style.borderBottom = "1px solid " + this.color;
+        this.input.style.background = "transparent";
+        this.input.style.width = this.width;
+        this.input.style.height = this.height;
+        this.input.style.fontSize = this.fontSize;
 
+        this.label.style.color = this.color;
+        this.label.style.position = "absolute";
+        this.label.style.transition = "transform .3s ease-out";
+        this.label.style.pointerEvents = "none";
+        this.label.style.transform = "translateY(0em)";
+    }
+    styleHover() {
+        this.label.style.transform = "translateY(-1.5em)";
+    }
+    clearValue() {
+        this.input.value = "";
+    }
+
+    eventHandler() {
+        this.input.addEventListener("mouseover", () => {
+            this.styleHover();
+        });
+        this.input.addEventListener("mouseout", () => {
+            if (this.input !== document.activeElement && !this.input.value) {
+                this.styleDefault();
+            }
+        });
+        this.input.addEventListener("blur", () => {
+            if (!this.input.value) {
+                this.styleDefault();
+            }
+        });
+    }
+
+    setID() {
+        let _random =
+            Math.random() * (Date.now() - (Date.now() - 2000)) +
+            (Date.now() - 2000);
+        if (this.element.id) {
+            let _id = this.element.id + Math.round(_random);
+            return _id;
+        } else if (this.element.className) {
+            let _id = this.element.className + Math.round(_random);
+            return _id;
+        } else {
+            return _random;
+        }
+    }
+    getKey() {
+        if (sessionStorage.length) {
+            this.keyCounter = sessionStorage.length;
+        }
+        if (this.element.id) {
+            let _key = this.element.id + sessionStorage.length;
+            this.keyCounter++;
+            return _key;
+        } else if (this.element.className) {
+            let _key = this.element.id + sessionStorage.length;
+            this.keyCounter++;
+            return _key;
+        } else {
+            this.keyCounter++;
+            return sessionStorage.length;
+        }
+    }
+    saveToSession() {
+        sessionStorage.setItem(this.getKey(), this.input.value);
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,57 @@
-let magicball = document.querySelector(".ball");
+let magicball = document.getElementById("theball");
 let shadow = document.querySelector(".shadow");
 let animbutton = document.getElementById("animationToggle");
+let theQuestion = document.getElementById("theQuestion");
+
+let testest = document.querySelector(".testball");
 
 let animBTN = new JSCheckBox(animbutton, 1, "Animation </br> Active", "Animation </br> Inactive");
 animBTN.initialize();
+
+class MagicBall {
+    constructor(_element, _index) {
+        this.element = _element;
+        this.index = _index;
+        this.id = "magicball" + this.index;
+        this.img = document.createElement("img");
+        this.src = `./img/magic8ball_${this.index}.png`
+    }
+    initialize() {
+        this.element.appendChild(this.img);
+        this.img.setAttribute("id", this.id);
+        document.getElementById(this.id).src = this.src;
+        this.style();
+    }
+    reDraw() {
+        document.getElementById(this.id).src = this.src;
+    }
+    style() {
+        // this.img.style.position = "absolute";
+        this.img.style.height = "100%";
+        this.img.style.width = "100%"
+        this.img.style.zIndex = "1"
+    }
+}
+
+console.log(magicball)
+
+function randomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let startBall = new MagicBall(magicball, "start");
+startBall.initialize();
+
+let arr = [];
+
+for (let i = 1; i <= 20; i++) {
+    let _src = `./img/magic8ball_${i}.png`
+    arr.push(_src);
+}
+
+console.log(arr);
 
 animbutton.addEventListener("click", () => {
     if (animBTN.input.checked) {
@@ -14,3 +62,23 @@ animbutton.addEventListener("click", () => {
         shadow.style.animation = "pulse 6s ease-in-out infinite";
     }
 })
+
+let question = new JSInput(
+    theQuestion,
+    "Ask your Question and the Ord will Decide",
+    "var(--textColorPrimary)",
+    "300px",
+    "",
+    "1em"
+);
+
+question.initialize();
+
+theQuestion.addEventListener("submit", (event) => {
+    console.log("event TRIGGER");
+    event.preventDefault();
+    startBall.src = arr[randomNumber(1, 20)];
+    startBall.reDraw();
+    question.saveToSession();
+    theQuestion.reset();
+});
